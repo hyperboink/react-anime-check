61dcccf7a98f6c22a5084c5d5e237ccce235c45a
hyperboink <hyperboink@gmail.com>
2021-08-29 13:57:35 +0800

    cleanup

--- src/components/utils/Utils.js
+++ src/components/utils/Utils.js
@@ -7,17 +7,17 @@ export const baseUrl = () => {
 }
 
 export const getParameterByName = (name, url) => {
-    if (!url) url = window.location.href;
+    if (!url) url = window.location.href
 
-    url = url.toLowerCase(); 
+    url = url.toLowerCase()
     name = name.replace(/[\[\]]/g, "\\$&").toLowerCase(); //eslint-disable-line
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
+        results = regex.exec(url)
 
-    if (!results) return null;
-    if (!results[2]) return '';
+    if (!results) return null
+    if (!results[2]) return ''
 
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+    return decodeURIComponent(results[2].replace(/\+/g, " "))
 }
 
 export const ifEmpty = (data, placeholder) => data || placeholder
